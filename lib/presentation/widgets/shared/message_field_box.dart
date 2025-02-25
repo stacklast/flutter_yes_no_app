d29ae0fe54de16e69c73971112fdf76d0e50cb7f
Edwin Benalcazar <softwareywebsoluciones@gmail.com>
2025-02-24 23:11:26 -0500

    feat: FormField behaviour

--- lib/presentation/widgets/shared/message_field_box.dart
+++ lib/presentation/widgets/shared/message_field_box.dart
@@ -5,30 +5,38 @@ class MessageFieldBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final colors = Theme.of(context).colorScheme;
+    final textController = TextEditingController();
+    final focusNode = FocusNode();
+
     final outlineInputBorder = UnderlineInputBorder(
         borderSide: BorderSide(
           color: Colors.transparent,
         ),
         borderRadius: BorderRadius.circular(40));
+
     final inputDecoration = InputDecoration(
+        hintText: 'End your message with a "??"',
         enabledBorder: outlineInputBorder,
         focusedBorder: outlineInputBorder,
         filled: true,
         suffixIcon: IconButton(
           icon: const Icon(Icons.send_outlined),
           onPressed: () {
-            //print("Value of the textfield?")
+            final textValue = textController.value.text;
+            textController.clear();
           },
         ));
 
     return TextFormField(
+      onTapOutside: (event) {
+        focusNode.unfocus();
+      },
+      focusNode: focusNode,
+      controller: textController,
       decoration: inputDecoration,
       onFieldSubmitted: (value) {
-        //print('Sumbit Value $value');
-      },
-      onChanged: (value) {
-        //print('Changed Value $value');
+        textController.clear();
+        focusNode.requestFocus();
       },
     );
   }
