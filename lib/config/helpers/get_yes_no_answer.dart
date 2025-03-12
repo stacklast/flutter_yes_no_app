760e914e22179e781279839d186946e94e2fc438
Edwin Benalcazar <softwareywebsoluciones@gmail.com>
2025-03-11 23:09:46 -0500

    refactor: Improve Model

--- lib/config/helpers/get_yes_no_answer.dart
+++ lib/config/helpers/get_yes_no_answer.dart
@@ -10,13 +10,9 @@ class GetYesNoAnswer {
 
     final response = await _dio.get('https://yesno.wtf/api');
 
-    final yesNoModel = YesNoModel.fromJson(response.data);
+    final yesNoModel = YesNoModel.fromJsonMap(response.data);
 
-    return Message(
-      text: yesNoModel.answer,
-      fromWho: FromWho.hers,
-      imageUrl: yesNoModel.image,
-    );
+    return yesNoModel.toMessageEntity();
 
   }
 }
--- lib/infrastructure/models/yes_no_model.dart
+++ lib/infrastructure/models/yes_no_model.dart
@@ -1,21 +1,32 @@
+import 'package:yes_no_app/domain/entities/message.dart';
+
+/// https://app.quicktype.io/
 class YesNoModel {
-  String answer;
-  bool forced;
-  String image;
+    final String answer;
+    final bool forced;
+    final String image;
 
-  YesNoModel(
-    {
-      required this.answer,
-      required this.forced,
-      required this.image,
-    }
-  );
+    YesNoModel({
+        required this.answer,
+        required this.forced,
+        required this.image,
+    });
 
-  factory YesNoModel.fromJson(Map<String, dynamic> json) => 
-  YesNoModel(
-    answer: json['answer'],
-    forced: json['forced'],
-    image: json['image'],
-  );
+    factory YesNoModel.fromJsonMap(Map<String, dynamic> json) => YesNoModel(
+        answer: json["answer"],
+        forced: json["forced"],
+        image: json["image"],
+    );
 
+    Map<String, dynamic> toJson() => {
+        "answer": answer,
+        "forced": forced,
+        "image": image,
+    };
+
+    Message toMessageEntity() => Message(
+      text: answer == 'yes' ? 'Yes' : 'No',
+      fromWho: FromWho.hers,
+      imageUrl: image,
+    );
 }
