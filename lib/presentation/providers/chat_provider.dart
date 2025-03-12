704f815b5f932ee590313f4523ff61fc7c8af166
Edwin Benalcazar <softwareywebsoluciones@gmail.com>
2025-03-11 21:28:20 -0500

    Feat: Show messages from provider

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
-  List<Message> message = [
+  List<Message> messageList = [
     Message(
       text: 'Hello!',
       fromWho: FromWho.me,
