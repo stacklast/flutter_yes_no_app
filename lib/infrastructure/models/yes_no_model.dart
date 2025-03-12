9aec9d1802aa49b1099b2d29a9f14e84e2f3bba7
Edwin Benalcazar <softwareywebsoluciones@gmail.com>
2025-03-11 22:58:01 -0500

    feat: Add Mapper

---
+++ lib/infrastructure/models/yes_no_model.dart
@@ -0,0 +1,21 @@
+class YesNoModel {
+  String answer;
+  bool forced;
+  String image;
+
+  YesNoModel(
+    {
+      required this.answer,
+      required this.forced,
+      required this.image,
+    }
+  );
+
+  factory YesNoModel.fromJson(Map<String, dynamic> json) => 
+  YesNoModel(
+    answer: json['answer'],
+    forced: json['forced'],
+    image: json['image'],
+  );
+
+}
--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -38,6 +38,9 @@ class ChatProvider extends ChangeNotifier {
 
   Future<void> herReply() async {
     final herMessage = await getYesNoAnswer.getAsnwer();
+    messageList.add(herMessage);
+    
+    moveScrollToBottom();
   }
 
   void moveScrollToBottom() async {
